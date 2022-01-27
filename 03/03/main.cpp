663789e2e6f79a3c6d1931bdc6c5724c4bae94fe
Kateryna Trofymenko <ekaterinatrofa@gmail.com>
2022-01-27 13:28:14 +0200

    update

--- 03/03/main.cpp
+++ 03/03/main.cpp
@@ -4,70 +4,55 @@
 #include <vector>
 #include <functional>
 #include <iomanip>
-#include "lab_03.h"
+#include <map>
+#include "lab.h"
 using namespace std;
 
 
 
-//random_device rd;  //Will be used to obtain a seed for the random number engine
-//mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-mt19937 rand_gen(1);
-ostream& operator<<(ostream& o, vector<double> v)
+random_device rdn;  //Will be used to obtain a seed for the random number engine
+mt19937 mt_generator(rdn()); //Standard mersenne_twister_engine seeded with rd()
+
+
+
+/**
+matyas function
+**/
+
+auto matyas = [](vector <double> v)
+
 {
-    for (auto e : v)
-    {
-        o << std::fixed << std::setprecision(5) << " " << e;
-    }
-    return o;
-}
+    double x = v.at(0), y = v.at(1);
+    return 0.26 * (pow(x, 2) + pow(y, 2))- 0.48*x*y;
+};
 
-//vector<double> hill_climbing(function<double(vector<double>)> f, function<bool(vector<double>)> f_domain, vector<double> p0, int iterations)
-//{
-//    auto p = p0;
-//
-//    uniform_int_distribution<> distrib(0, p.size() - 1);
-//    uniform_real_distribution<> distrib_r(-0.1, 0.1);
-//
-//    if (!f_domain(p)) throw std::invalid_argument("The p0 point must be in domain");
-//
-//    //iterations of algorithm
-//    for (int i = 0; i < iterations; i++)
-//    {
-//
-//        //candidate
-//        auto p2 = p;
-//
-//
-//
-//        p2[distrib(gen)] += distrib_r(gen);
-//
-//        if(!f_domain(p2))
-//        {
-//            //evaluate candidate point
-//            double y2 = f(p2);
-//
-//            //check if keep new point
-//            if (y2 < f(p))
-//            {
-//                //store new point
-//                p = p2;
-////            cout << p << " " << f(p) << endl;
-//            }
-//        }
-//        else
-//        {
-//            cout << "The current p2 point must be in domain" << endl;
-//        }
-//
-//
-//
-//
-//
-//    }
-//    return p;
-//}
+auto matyas_domain = [](vector <double> v)
 
+{
+    return (abs(v[0]) <= 10) && (abs(v[1]) <= 10);
+};
 
+/**
+
+eggholder function
+**/
+
+auto eggholder_function = [](vector <double> v)
+{
+
+    double x = v.at(0), y = v.at(1);
+
+    double kk = abs(x/2 + (y + 47));
+    double ll = abs(x - (y + 47));
+
+    return -(y + 47)*sin(sqrt(kk)) - x*sin(ll);
+
+};
+
+auto eggholder_function_domain = [](vector <double> v)
+{
+    return (abs(v[0]) <= 512) && (abs(v[1]) <= 512);
+};
 
 int main()
 {
@@ -75,24 +60,28 @@ int main()
     int choice;
     do
     {
-
         cout << "Please select the function: "<< endl;
         cout << endl;
         cout << "1 -------> Matyas function " << endl;
-        cout << "2 -------> Schaffer function N. 2" << endl;
-        cout << "3 -------> Eggholder function" << endl;
-        cout << "4 -------> Quit" << endl;
+        cout << "2 -------> Eggholder function" << endl;
+        cout << "3 -------> Quit" << endl;
         cout << endl;
-        cout << "Enter 1, 2, 3, or 4: " << endl;
+        cout << "Enter 1, 2 or 3: " << endl;
+
+        auto foo_matyas = matyas;
+        auto mat_dom = matyas_domain;
+
+        auto foo_egg = eggholder_function;
+        auto egg_dom = eggholder_function_domain;
 
         cin >>choice;
 
 
-        while (choice < 1 || choice > 4)
+        while (choice < 1 || choice > 3)
         {
             cout << "Please enter a valid choice" << endl;
 
-            cout << "Enter 1, 2, 3, or 4: " << endl;
+            cout << "Enter 1, 2 or 3: " << endl;
 
             cin >> choice;
         }
@@ -105,159 +94,352 @@ int main()
             cout << "You have selected Matyas function"<< endl;
             cout << endl;
 
-            cout << sum(2,3) << endl;
+            cout << "Please choose the algorithm: " << endl;
 
+            cout << "1 -------> Hill Climbing " << endl;
+            cout << "2 -------> Simulated annealing " << endl;
+            cout << "3 -------> Compare Hill Climbing and Simulated annealing" << endl;
 
+            int choice_1;
+            cin >> choice_1;
 
-            auto matyas = [](vector <double> v)
-
+            switch(choice_1)
             {
-                double x = v.at(0), y = v.at(1);
-                return 0.26 * (pow(x, 2) + pow(y, 2))- 0.48*x*y;
-            };
 
-            auto matyas_domain = [](vector <double> v)
+            case 1:
             {
-                return (abs(v[0]) <= 10) && (abs(v[1]) <= 10);
-            };
 
+                cout << "You have selected Hill Climbing" << endl;
+                cout << endl;
 
-            uniform_real_distribution<> distrib_r(-10, 10);
+                uniform_real_distribution<> distrib_r(-10, 10);
 
-            //punkt początkowy losowany z dziedziny [-10;10]
+                //punkt początkowy losowany z dziedziny [-10;10]
 
-            vector<double> matyas_p0 =
+                vector<double> matyas_p0 =
+                {
+                    distrib_r(mt_generator),
+                    distrib_r(mt_generator),
+
+                };
+
+                int n;
+
+                cout << "Please enter the number of iterations for function  " << endl;
+                cin >> n;
+
+                auto result_matyas = hill_climbing(foo_matyas, mat_dom, matyas_p0, n);
+
+                cout << "f(" << result_matyas << ") = "<< " " << foo_matyas(result_matyas) << endl;
+                cout << endl;
+
+                break;
+
+            }
+
+            case 2:
             {
-                distrib_r(rand_gen),
-                distrib_r(rand_gen),
-            };
+
+                cout << "You have selected Simulated annealing " << endl;
+                cout << endl;
+
+                uniform_real_distribution<> distrib_r(-10, 10);
+
+                //punkt początkowy losowany z dziedziny [-10;10]
+
+                vector<double> matyas_p0 =
+                {
+                    distrib_r(mt_generator),
+                    distrib_r(mt_generator),
+
+                };
+
+                int n;
+
+                cout << "Please enter the number of iterations for function  " << endl;
+                cin >> n;
+                auto result_matyas= simulated_annealing(foo_matyas, mat_dom, matyas_p0, n,
+                                                        [](auto p)
+                {
+                    normal_distribution<double> n(0.0, 0.3);
+                    for (auto& e : p)
+                    {
+                        e = e + n(mt_generator);
+                    }
+                    return p;
+                },
+
+                [](int k)
+                {
+                    return 1000.0 / k;
+                });
+
+                cout << "f(" << result_matyas << ") = "<< " " << foo_matyas(result_matyas) << endl;
+                cout << endl;
+                break;
+            }
+
+            case 3:
+            {
+
+                cout << "Comparing HC & SA" << endl;
+                cout << "Provide the number of iterations for tests" << endl;
+                int ni;
+
+                cin >> ni;
 
 
-            int n;
 
-            cout << "Please enter the number of iterations for function  " << endl;
+                int hc_sa = 0;
 
-            cin >>n;
 
-            auto result_matyas= hill_climbing(matyas, matyas_domain,matyas_p0, n);
 
-            cout << "f(" << result_matyas << ") = "<< " " << matyas(result_matyas) << endl;
-            cout << endl;
+                for (int i = 0; i < ni; i++)
+                {
+
+
+                    int itr = 1000;
+
+
+                    auto f = matyas;
+
+                    uniform_real_distribution<> distrib_r(-10, 10);
+
+
+
+                    vector<double> matyas_p0 =
+                    {
+                        distrib_r(mt_generator),
+                        distrib_r(mt_generator),
+
+                    };
+                    auto matyas_hc = hill_climbing(f, mat_dom, matyas_p0, itr);
+
+                    auto matyas_sa= simulated_annealing(f, mat_dom, matyas_p0, itr,
+                                                        [](auto p)
+                    {
+                        normal_distribution<double> n(0.0, 0.3);
+                        for (auto& e : p)
+                        {
+                            e = e + n(mt_generator);
+                        }
+                        return p;
+                    },
+
+                    [](int k)
+                    {
+                        return 1000.0 / k;
+                    });
+
+                    cout << "Hill climbing:           ";
+                    cout << " result = " << f(matyas_hc) << " " << endl;
+
+                    cout << "simmulated annealing: ";
+                    cout << " result = " << f(matyas_sa) << " " << endl;
+
+
+
+                    if (f(matyas_hc) > f(matyas_sa))
+                    {
+                        hc_sa++;
+                    }
+                }
+
+                cout << "Simmulated annealing was better " << hc_sa << " times " << "from " << ni << " iterations"<<endl;
+
+
+
+                break;
+            }
+
+            }
             break;
-        }
 
+        }
 
         case 2:
         {
-            cout << "You have selected Schaffer function N. 2" << endl;
-            cout << endl;
-            auto Schaffer_function_N_2 = [](vector <double> v)
-            {
-
-                double x = v.at(0), y = v.at(1);
-
-                double xx = pow(x,2)-pow(y,2); // x^2 - y^2
-
-                double sin_2 = 0.5*(1 - cos(2 * xx));
-                double numerator = sin_2 - 0.5;
-
-                double denominator = pow((1+0.001*(pow(x,2)+ pow(y,2)) ),2);
-                return 0.5 + (numerator/denominator);
-
-            };
-
-
-            auto Schaffer_function_N_2_domain = [](vector <double> v)
-            {
-                return (abs(v[0]) <= 100) && (abs(v[1]) <= 100);
-            };
-
-
-            uniform_real_distribution<> distrib_r(-100, 100);
-
-            //punkt początkowy losowany z dziedziny [-100;100]
-
-            vector<double> Schaffer_function_N_2_p0 =
-            {
-                distrib_r(rand_gen),
-                distrib_r(rand_gen),
-            };
-
-            int n;
-
-            cout << "Please enter the number of iterations for function  " << endl;
-
-            cin >>n;
-
-            auto result_Schaffer_function_N_2= hill_climbing(Schaffer_function_N_2, Schaffer_function_N_2_domain,Schaffer_function_N_2_p0, n);
-
-            cout << "f(" << result_Schaffer_function_N_2 << ") = "<< " " << Schaffer_function_N_2(result_Schaffer_function_N_2) << endl;
-            cout << endl;
-            break;
-
-        }
-
-
-        case 3:
-        {
             cout << "You have selected Eggholder function" << endl;
             cout << endl;
-            auto eggholder_function = [](vector <double> v)
+
+            cout << "Please choose the algorithm: " << endl;
+
+            cout << "1 -------> Hill Climbing " << endl;
+            cout << "2 -------> Simulated annealing " << endl;
+            cout << "3 -------> Compare Hill Climbing and Simulated annealing" << endl;
+
+
+
+            int choice_2;
+            cin >> choice_2;
+
+            switch(choice_2)
             {
 
-                double x = v.at(0), y = v.at(1);
-
-                double kk = abs(x/2 + (y + 47));
-                double ll = abs(x - (y + 47));
-
-                return -(y + 47)*sin(sqrt(kk)) - x*sin(ll);
-
-            };
-
-
-            auto eggholder_function_domain = [](vector <double> v)
+            case 1:
             {
-                return (abs(v[0]) <= 512) && (abs(v[1]) <= 512);
-            };
 
+                cout << "You have selected Hill Climbing" << endl;
+                cout << endl;
 
-            uniform_real_distribution<> distrib_r(-512, 512);
+                uniform_real_distribution<> distrib_r(-512, 512);
 
-            //punkt początkowy losowany z dziedziny [-100;100]
+                //punkt początkowy losowany z dziedziny [-512;512]
 
-            vector<double> eggholder_function_p0 =
+                vector<double> eggholder_function_p0 =
+                {
+                    distrib_r(mt_generator),
+                    distrib_r(mt_generator),
+                };
+
+                int n;
+
+                cout << "Please enter the number of iterations for function  " << endl;
+                cin >> n;
+
+                auto result_eggholder = hill_climbing(foo_egg, egg_dom, eggholder_function_p0, n);
+
+                cout << "f(" << result_eggholder << ") = "<< " " << foo_egg(result_eggholder) << endl;
+                cout << endl;
+
+                break;
+
+            }
+
+            case 2:
             {
-                distrib_r(rand_gen),
-                distrib_r(rand_gen),
-            };
 
-            int n;
+                cout << "You have selected Simulated annealing " << endl;
+                cout << endl;
 
-            cout << "Please enter the number of iterations for function  " << endl;
+                uniform_real_distribution<> distrib_r(-512, 512);
 
-            cin >>n;
+                //punkt początkowy losowany z dziedziny [-512;512]
 
-            auto result_eggholder_function= hill_climbing(eggholder_function, eggholder_function_domain,eggholder_function_p0, n);
+                vector<double> eggholder_function_p0 =
+                {
+                    distrib_r(mt_generator),
+                    distrib_r(mt_generator),
+                };
 
-            cout << "f(" << result_eggholder_function << ") = "<< " " << eggholder_function(result_eggholder_function) << endl;
-            cout << endl;
+
+                int n;
+
+                cout << "Please enter the number of iterations for function  " << endl;
+                cin >> n;
+                auto result_eggholder= simulated_annealing(foo_egg, egg_dom, eggholder_function_p0, n,
+                                       [](auto p)
+                {
+                    normal_distribution<double> n(0.0, 0.3);
+                    for (auto& e : p)
+                    {
+                        e = e + n(mt_generator);
+                    }
+                    return p;
+                },
+
+                [](int k)
+                {
+                    return 1000.0 / k;
+                });
+
+                cout << "f(" << result_eggholder << ") = "<< " " << foo_egg(result_eggholder) << endl;
+                cout << endl;
+                break;
+            }
+
+
+            case 3:
+            {
+                cout << "Comparing HC & SA" << endl;
+                cout << "Provide the number of iterations for tests" << endl;
+                int ni;
+
+                cin >> ni;
+
+
+
+                int hc_sa = 0;
+
+
+
+                for (int i = 0; i < ni; i++)
+                {
+
+
+                    int itr = 100000;
+
+
+                    auto f1 = eggholder_function;
+
+                    uniform_real_distribution<> distrib_r(-512, 512);
+
+
+                    vector<double> eggholder_function_p0 =
+                    {
+                        distrib_r(mt_generator),
+                        distrib_r(mt_generator),
+                    };
+                    auto egg_hc = hill_climbing(f1, egg_dom, eggholder_function_p0, itr);
+
+                    auto egg_sa= simulated_annealing(f1, egg_dom, eggholder_function_p0, itr,
+                                                     [](auto p)
+                    {
+                        normal_distribution<double> n(0.0, 0.3);
+                        for (auto& e : p)
+                        {
+                            e = e + n(mt_generator);
+                        }
+                        return p;
+                    },
+
+                    [](int k)
+                    {
+                        return 1000.0 / k;
+                    });
+
+                    cout << "Hill climbing:           ";
+                    cout << " result = " << f1(egg_hc) << " " << endl;
+
+                    cout << "simmulated annealing: ";
+                    cout << " result = " << f1(egg_sa) << " " << endl;
+
+
+                    if (f1(egg_hc) > f1(egg_sa))
+                    {
+                        hc_sa++;
+                    }
+                }
+
+                cout << "Simmulated annealing was better " << hc_sa << " times " << "from " << ni << " iterations"<<endl;
+
+
+                break;
+            }
+
+
+
             break;
 
+            }
+
+            break;
         }
 
-        case 4:
-            cout << "Goodbye" << endl;
-            cout << endl;
+            case 3:
+                cout << "Goodbye" << endl;
+                cout << endl;
+                break;
+            }
+
         }
+        while (choice <= 2 && choice >= 1);
+
+
+        return 0;
+
 
     }
-    while (choice <= 3 && choice >= 1);
-
-
-    return 0;
-
-
-}
 
 
 
--- 05/ga.h
+++
@@ -1,6 +0,0 @@
-#ifndef GA_H_INCLUDED
-#define GA_H_INCLUDED
-
-
-
-#endif // GA_H_INCLUDED
